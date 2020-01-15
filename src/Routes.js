98850c7515e3961ab0dffb196b2be68e0b594169
Zening Luo <zenluo@students.lfanet.org>
2020-01-15 15:38:42 -0600

    login setup

--- src/Routes.js
+++ src/Routes.js
@@ -2,13 +2,16 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
+import AppliedRoute from "./components/AppliedRoute";
 
-export default function Routes() {
-  return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-      { /* Finally, catch all unmatched routes */ }
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
+export default function Routes({ appProps }) {
+    return (
+      <Switch>
+        <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+        <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+        { /* Finally, catch all unmatched routes */ }
+        <Route component={NotFound} />
+      </Switch>
+    );
+  }
---
+++ src/containers/Login.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import "./Login.css";
+import { Auth } from "aws-amplify";
+import LoaderButton from "../components/LoaderButton";
+
+export default function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+  
+    setIsLoading(true);
+  
+    try {
+      await Auth.signIn(email, password);
+      props.userHasAuthenticated(true);
+      props.history.push("/");
+    } catch (e) {
+      alert(e.message);
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <div className="Login">
+      <form onSubmit={handleSubmit}>
+        <FormGroup controlId="email" bsSize="large">
+          <ControlLabel>Email</ControlLabel>
+          <FormControl
+            autoFocus
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup controlId="password" bsSize="large">
+          <ControlLabel>Password</ControlLabel>
+          <FormControl
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            type="password"
+          />
+        </FormGroup>
+           <LoaderButton
+             block
+             type="submit"
+             bsSize="large"
+             isLoading={isLoading}
+             disabled={!validateForm()}
+            >
+            Login
+            </LoaderButton>
+      </form>
+    </div>
+  );
+}
