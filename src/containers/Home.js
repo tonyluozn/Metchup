371f1c9d9ed6934b368efe06c0e0be9026ea8448
Zening Luo <zenluo@students.lfanet.org>
2020-01-15 14:29:58 -0600

    Set up Routesand containers

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Metchup</h1>
+        <p>A simple way to find your study partner</p>
+      </div>
+    </div>
+  );
+}
